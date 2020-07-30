033398294cd4d4fdd36ef95da32a736c916468f4
Gustavo Lima <gustavo.coelho.lima@gmail.com>
2020-07-31 00:56:03 +0100

    Added styles to auth client pages

--- client/src/components/auth/Login.svelte
+++ client/src/components/auth/Login.svelte
@@ -18,16 +18,30 @@
     }
 </script>
 
-<form class="container">
-    <div class="form-group d-flex flex-column justify-content-center">
-        <label for="username">
-            Username
-            <input type="text" id="username" bind:value={username}/>
-        </label>    
-        <label for="password1">
-            Password
-            <input type="password" id="password1" bind:value={password} />
-        </label>
-        <button class="btn btn-primary" on:click={login}>Login</button>
+<style>
+    .container {
+        padding-top: 5rem;
+    }
+
+    input {
+        margin-bottom: 1rem;
+    }
+
+    button[type=submit] {
+        margin-top: 1rem;
+    }
+</style>
+
+<div class="container">
+    <div class="w-50 mx-auto form-group row">
+        <label for="username" class="col-sm-2 col-form-label">Username</label>
+        <div class="col-sm-10">
+            <input type="text" class="form-control" id="username" bind:value={username}>
+        </div>
+        <label for="password" class="col-sm-2 col-form-label">Password</label>
+        <div class="col-sm-10">
+            <input type="password" class="form-control" id="password" bind:value={password}>
+        </div>
+        <button type="submit" class="mx-auto w-100 btn btn-primary" on:click={login}>Login</button>
     </div>
-</form>
+</div>
--- client/src/components/auth/Register.svelte
+++ client/src/components/auth/Register.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { navigate } from "svelte-routing";
     import { urls } from '../../config';
 
     export let location;
@@ -15,22 +16,39 @@
         });
         const result = await res.json();
         console.log(result);
+        if (result.error !== undefined) navigate("/", { replace: true });
     }
     
 </script>
 
-<div>
-    <label for="username">
-        Username
-        <input type="text" id="username" bind:value={username}/>
-    </label>    
-    <label for="password1">
-        Password
-        <input type="password" id="password1" bind:value={password1} />
-    </label>
-    <label for="password2">
-        Repeat password
-        <input type="password" id="password2" bind:value={password2} />
-    </label>
-    <button class="btn btn-primary" on:click={register}>Register</button>
+<style>
+    .container {
+        padding-top: 5rem;
+    }
+
+    input {
+        margin-bottom: 1rem;
+    }
+
+    button[type=submit] {
+        margin-top: 1rem;
+    }
+</style>
+
+<div class="container">
+    <div class="w-50 mx-auto form-group row">
+        <label for="username" class="col-sm-2 col-form-label">Username</label>
+        <div class="col-sm-10">
+            <input type="text" class="form-control" id="username" bind:value={username}>
+        </div>
+        <label for="password1" class="col-sm-2 col-form-label">Password</label>
+        <div class="col-sm-10">
+            <input type="password" class="form-control" id="password1" bind:value={password1}>
+        </div>
+        <label for="password2" class="col-sm-2 col-form-label">Repeat password</label>
+        <div class="col-sm-10">
+            <input type="password" class="form-control" id="password2" bind:value={password2}>
+        </div>
+        <button type="submit" class="mx-auto w-100 btn btn-primary" on:click={register}>Login</button>
+    </div>
 </div>
