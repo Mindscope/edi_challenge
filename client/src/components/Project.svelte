99bd5cdb52924510273c87d7529d5fcc81677cea
Gustavo Lima <gustavo.coelho.lima@gmail.com>
2020-07-30 20:33:44 +0100

    Separated task logic from Project component

--- client/src/components/Project.svelte
+++ client/src/components/Project.svelte
@@ -1,90 +1,135 @@
 <script>
-    import { createEventDispatcher } from "svelte";
-    const baseUrl = "http://localhost:8081/api/projects";
-    const dispatch = createEventDispatcher();
-    export let project;
-    let tasks = [];
-    let newTaskDescription;
+  import { createEventDispatcher } from "svelte";
+  import Task from "./Task.svelte";
 
-    async function deleteProject(event) {
-        const projectId = project._id;
-        const url = baseUrl + `/${project._id}`;
-        const res = await fetch(url, {method: "DELETE"});
-        dispatch("deleted", {name: projectId});
+  const baseUrl = "http://localhost:8081/api/projects";
+  const dispatch = createEventDispatcher();
+  export let project;
+  let doneTasks = [],
+    todoTasks = [];
+  let newTaskDescription;
+
+  async function deleteProject(event) {
+    const projectId = project._id;
+    const url = baseUrl + `/${project._id}`;
+    const res = await fetch(url, { method: "DELETE" });
+    dispatch("deleted", { name: projectId });
+  }
+
+  async function updateTasks() {
+    console.log(`Fetching tasks for ${project._id}`);
+    const url = baseUrl + `/${project._id}/tasks`;
+    let res = await fetch(url);
+    res.json().then((tasks) => sortTasks(tasks));
+  }
+
+  function sortTasks(tasks) {
+    doneTasks = [];
+    todoTasks = [];
+    if (tasks.length > 0) {
+      tasks.forEach((task) => {
+        if (task.is_complete) doneTasks.push(task);
+        else todoTasks.push(task);
+      });
     }
 
-    async function updateTasks() {
-        console.log(`Fetching tasks for ${project._id}`);
-        const url = baseUrl + `/${project._id}/tasks`;
-        let res = await fetch(url);
-        tasks = await res.json();
-        console.log(tasks);        
+    // Setup tooltips for done tasks elements
+    window.$("[data-toggle]").tooltip();
+  }
+
+  async function addTask() {
+    if (newTaskDescription === undefined) {
+      console.error("No task description was given.");
+      return;
     }
 
-    async function addTask() {
-        if (newTaskDescription === undefined) {
-            console.error("No task description was given.");
-            return;
-        }
+    const url = baseUrl + `/${project._id}/tasks`;
+    let res = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({ description: newTaskDescription }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    res.json().then((tasks) => sortTasks(tasks));
+    newTaskDescription = "";
+  }
 
-        const url = baseUrl + `/${project._id}/tasks`;
-        let res = await fetch(url, {
-            method: "POST", 
-            body: JSON.stringify({description: newTaskDescription}),
-            headers: {
-                'Content-Type': 'application/json'                
-            }
-        });
-        tasks = await res.json();        
-    }
+  async function deleteTask(event) {
+    const id = event.detail.id;
+    const url = baseUrl + `/${project._id}/tasks/${id}`;
+    const res = await fetch(url, { method: "DELETE" });
+    res.json().then((tasks) => sortTasks(tasks));
+  }
 
-    async function deleteTask(id) {
-        const url = baseUrl + `/${project._id}/tasks/${id}`;
-        const res = await fetch(url, {method: "DELETE"});
-        tasks = await res.json();
-    }
+  async function finishTask(event) {
+    const id = event.detail.id;
+    const url = baseUrl + `/${project._id}/tasks/${id}`;
+    const res = await fetch(url, {
+      method: "PUT",
+      body: JSON.stringify({ is_complete: true }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    res.json().then((tasks) => sortTasks(tasks));
+  }
 
-    async function finishTask(id) {
-        const url = baseUrl + `/${project._id}/tasks/${id}`;
-        const res = await fetch(url, {
-            method: "PUT", 
-            body: JSON.stringify({is_complete: true}),
-            headers: {
-                'Content-Type': 'application/json'                
-            }
-        });
-        tasks = await res.json();
-    }
+  async function updateTask(event) {
+    const task = event.detail;
+    const url = baseUrl + `/${project._id}/tasks/${task._id}`;
+    const res = await fetch(url, {
+      method: "PUT",
+      body: JSON.stringify(task),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    res.json().then((tasks) => sortTasks(tasks));
+  }
 
-    $: project && updateTasks();
+  $: project && updateTasks();
 </script>
 
+<style>
+  .task-list {
+    margin-bottom: 2rem;
+  }
+</style>
 
 {#if project}
-<div class="card">
-    <div class="card-header">
-        {project.name}
-        <button class="btn btn-info">Edit</button>
-        <button class="btn btn-danger" on:click={deleteProject}>Delete</button>
+  <div class="card">
+    <div class="card-header d-flex">
+      {project.name}
+      <div class="ml-auto">
+        <i class="tool fas fa-edit" />
+        <i class="tool far fa-trash-alt" on:click={deleteProject} />
+      </div>
     </div>
     <div class="card-body">
-        {#if tasks.length > 0}
-        <h3>To Do</h3>
-        <ul>
-            {#each tasks as task}
-            <li>
-                <input type="checkbox" checked={task.is_complete} on:click={() => finishTask(task._id)}/>
-                {task.description}
-                <button class="btn btn-danger" on:click={() => deleteTask(task._id)}>Remove</button>
-            </li>        
-            {/each}
-        </ul>
-        <hr />
-        {/if}
-        <div>
-            <input type="text" placeholder="Task Description" bind:value={newTaskDescription}/>
-            <button class="btn btn-success" on:click={addTask}>Add</button>
+      {#if todoTasks.length > 0}
+        <h4>To Do</h4>
+        <div class="task-list">
+          {#each todoTasks as task}
+            <Task {task} on:delete={deleteTask} on:finish={finishTask} on:update={updateTask}/>
+          {/each}
         </div>
+      {/if}
+      {#if doneTasks.length > 0}
+        <h4>Done</h4>
+        <div class="task-list">
+          {#each doneTasks as task}
+            <Task {task} />
+          {/each}
+        </div>        
+      {/if}
     </div>
-</div>
+    <div class="card-footer">
+      <input
+        type="text"
+        placeholder="Task Description"
+        bind:value={newTaskDescription} />
+      <button class="btn btn-success" on:click={addTask}>Add</button>
+    </div>
+  </div>
 {/if}
---
+++ client/src/components/Task.svelte
@@ -0,0 +1,53 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+  export let task;
+  let isEditing = false;
+
+  function toggleEdit() {
+    isEditing = true;
+  }
+
+  function saveTask() {
+    isEditing = false;
+    dispatch("update", task);
+  }
+</script>
+
+<style>
+  .completed-desc {
+    padding-left: 0.5rem;
+  }
+</style>
+
+{#if task}
+  <div class="d-flex align-items-center">
+    {#if !task.is_complete}
+      <input
+        type="checkbox"
+        checked={task.is_complete}
+        on:click={() => dispatch('finish', { id: task._id })} />
+      {#if isEditing}
+        <input type="text" bind:value={task.description} />        
+      {:else}
+        <span class="pl-2 pr-3">{task.description}</span>
+      {/if}
+      <div class="ml-auto">
+        {#if !isEditing}<i class="tool fas fa-edit" on:click={toggleEdit} />
+        {:else}<i class="fas fa-check-square" on:click={saveTask} />{/if}
+        <i
+          class="tool far fa-trash-alt"
+          on:click={() => dispatch('delete', { id: task._id })} />
+      </div>
+    {:else}
+      <input type="checkbox" checked={task.is_complete} disabled="true" />
+      <span
+        class="completed-desc"
+        data-toggle="tooltip"
+        title={task.finished_at}>
+        {task.description}
+      </span>
+    {/if}
+  </div>
+{/if}
